d3b8c72ee3557e868e8b09ebe796844e8826a834
Macintosh HD <macintoshhd@Macintoshui-MacBook-Pro.local>
2019-10-25 11:36:32 +0900

    getMovie() take two args, limit, rating.

--- graphql/db.js
+++ graphql/db.js
@@ -1,7 +1,16 @@
 import fetch from "node-fetch";
 
-const API_URL = "https://yts.lt/api/v2/list_movies.json";
-export const getMovies = (limit, rating) =>
-  fetch(API_URL)
+const API_URL = "https://yts.lt/api/v2/list_movies.json?";
+export const getMovies = (limit, rating) => {
+  let REQ_API = API_URL;
+  if (limit > 0) {
+    REQ_API += `limit=${limit}`;
+  }
+  if (rating > 0) {
+    REQ_API += `&minimum_rating=${rating}`;
+  }
+  return fetch(REQ_API)
     .then(res => res.json())
     .then(json => json.data.movies);
+  //.then(json => console.log(json.data.movies));
+};
