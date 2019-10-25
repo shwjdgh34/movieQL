2dfdcea8eb36c8547e96f73e2c1499d8fa192d98
Macintosh HD <macintoshhd@Macintoshui-MacBook-Pro.local>
2019-10-25 10:13:38 +0900

    added person query that use getById function

--- graphql/db.js
+++ graphql/db.js
@@ -1,12 +1,19 @@
 export const people = [
   {
+    id: 1,
     name: "nick",
     age: 28,
     gender: "male"
   },
   {
+    id: 2,
     name: "nono",
     age: 18,
     gender: "female"
   }
 ];
+
+export const getById = id => {
+  const filteredPeople = people.filter(person => person.id === id);
+  return filteredPeople[0];
+};
